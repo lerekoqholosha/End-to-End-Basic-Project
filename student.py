f7985627665430105cb1e65443101bff74a4aa69
Lereko Qholosha <lerekoqholosha9@gmail.com>
2022-04-08 01:38:23 +0200

    Update student.py

--- student.py
+++ student.py
@@ -1,20 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 24 09:46:13 2022
+@author: Lereko Qholosha
+"""
+
 import streamlit as st
-import joblib
+import pickle
 import numpy as np
-import pandas as pd
 
-st.write('## Student Mark Prediction')
+def load_model():
+    with open('lr_model.pkl', 'rb') as file:
+        data = joblib.load(file)
+    return data
 
-marks=st.text_input("Enter Number of hours you study: ","")
 
-model = joblib.load('lr_model.pkl')
-ok = st.button("Calculate Marks")
-if ok:
-    X=np.array([[marks]])
-    X=X.astype(float)
+data = load_model()
 
-marks = model.predict(X)
-st.subheader(f"The estimated Marks are {marks[0]:.2f}")
+regressor_loaded = data["model"]
+hours = data["hours"]
+
+
+def show_predict_page():
+    st.title("Student's Score Prediction")
+    
+    st.write(""" ### We need some information to predict the marks """)
+    
+    marks = st.text_input("Enter Number of hours you study: ","")
+    
+    ok = st.button("Calculate Marks")
+    if ok:
+        X = np.array([[marks]])
+        X = X.astype(float)
+        
+        marks = regressor_loaded.predict(X)
+        st.subheader(f"The estimated Marks are {marks[0]:.2f}") 
 
 st.write('Made 😍 Lereko')
     
